56d67f4ef6b8e413b559c7bd01bb3cbbdc6da901
Ankit Shaji <ankitshaji@gmail.com>
2022-07-19 20:47:35 +1200

    value proprty of the 2 inputObjects when submitted

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 //Form eventStrings and eventObjects - Single page workflow - show data on current page,dont leave to new page location
 
-//Element object = (JSObject/ElementObject/NodeObject).method(css element selector)
+//elementObject = (JSObject/elementObject/nodeObject).method(css element selector)
 const tweetForm = document.querySelector("body #tweetForm");
 
-//ElementObject.method(eventString,anonymousCallbackFunctionExpression(eventObject))
+//elementObject.method(eventString,anonymousCallbackFunctionExpression(eventObject))
 //eventString -"submit"
 //eventObject - eg.submitEventObject-propeties/methods
 //by default form eventObject sends HTTPRequest with FormData to action attributes location
 //by default form eventObject moves onto next page location URL in action attribute
 
-//method - addEventListner(eventString,anonymousCallbackFunctionExpression(eventObject))
+//method - addEventListner(eventString,anonymousCallbackFunctionExpression(parameter))
 //when stringEvent happens on elementObject(Target)
 //it executes the anonymous callback function expression
-//when it executes the fucntion expression it create and passes in the evenObject
+//when it executes the fucntion expression it create and passes in the evenObject as argument
 //this eventObject is caught as a parameter in the function expression
 tweetForm.addEventListener("submit", function (e) {
   //submitEventObject.method()
@@ -20,5 +20,20 @@ tweetForm.addEventListener("submit", function (e) {
   //prevent deafult eventObject sending HttpRequest Formdata to action attribute location
   //prevent deafult eventObject moving onto next page location URL in action attribute
 
-  alert("Submit");
+  //basic way to get value property
+  //elementObject= nodeListObject[1]
+  // const input1 = document.querySelectorAll("input")[0];
+  // const input2 = document.querySelectorAll("input")[1];
+  // //elementObejct.property
+  // console.log(input1.value);
+  // console.log(input2.value);
+
+  //better way to get value property
+  //this keyword - Execution scope - left of dot - refers to formElementObject
+  //formElementObject.propery
+  //becomes - HTMLFormContorlsCollectionObject.property
+  //property is the value of name attribute of element
+  //becomes - inputObject.property - returns string
+  const username = this.elements.username.value;
+  const tweet = this.elements.tweet.value;
 });
